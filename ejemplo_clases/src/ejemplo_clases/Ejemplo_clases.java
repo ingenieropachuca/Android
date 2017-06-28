aec776e876520f01094bd6efdc2b2f2d1980dd38
ingenieropachuca <ingenieropachuca@gmail.com>
2017-06-27 23:47:29 -0500

    Ejercicio 2 usando clases
    Aun no esta terminado este ejercicio de clases

---
+++ ejemplo_clases/src/ejemplo_clases/Ejemplo_clases.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplo_clases;
+import ejemplo_clases.Tren;
+
+/**
+ *
+ * @author Carlos
+ */
+public class Ejemplo_clases {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ ejemplo_clases/src/ejemplo_clases/Tren.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplo_clases;
+
+/**
+ *
+ * @author Carlos
+ */
+public class Tren {
+    int vagones[][] =new int[5][2];
+    String maquinista;
+    int antiguedad;
+    
+}
